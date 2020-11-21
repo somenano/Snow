dd4f1a57879bd6145727f22a7c4b2f9a6fcd1dd3
Jason Pawlak <jasonapawlak@gmail.com>
2020-11-21 15:15:44 -0600

    Update flyby_listings.js

--- js/flyby_listings.js
+++ js/flyby_listings.js
@@ -7,24 +7,6 @@ var advert_listing = [
     {
         'text': 'Advertise here for 5 Nano per month',
         'url': 'https://somenano.com/advertise?ref=snow'
-    },
-    {
-        'text': 'NanoCenter.org - Venezuela Billboard',
-        'url': 'https://nanocenter.org/projects/a19be820-b953-11e9-aece-ff5bc83d23c6'
-    },
-    {
-        'text': 'NanoCenter.org - Twitch Distribution',
-        'url': 'https://nanocenter.org/projects/6402fc40-cc3a-11e9-b383-db34bbe175b8'
-    },
-    {
-        'text': 'NanoPixelWall- choose spot, upload image, share.',
-        'url': 'https://nanopixelwall.com',
-        'expires': 'Thu Jan 01 2020 16:30:26 GMT-0600 (Central Standard Time)'
-    },
-    {
-        'text': 'Turn on the NanoCenter Xmas lights with Nano now!',
-        'url': 'https://xmas.nanos.cc',
-        'expires': 'Sat Jan 04 2020 18:51:47 GMT-0600 (Central Standard Time)'
     }
 ];
 
